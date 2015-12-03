eaf4c2778298d9e6b458bc7999b62fe26414ec40
dsazer25 <dsazer25@gmail.com>
2015-12-02 21:33:26 -0600

    Added comments

--- Code/rss_reddit.py
+++ Code/rss_reddit.py
@@ -1,11 +1,21 @@
 #! /usr/bin/python
 
+#This .py file is not to be executed as an isolated script
+#This .py file will be imported into the PasswordCompare.py script 
+#It allows RSS feeds to be pulled from the internet and displayed 
+
+#import the "feedparser" open-source Python module 
 import feedparser
 
+#Parses RSS feeds from reddit subreddits 
+def subreddit(myInput):
+	d = feedparser.parse("http://www.reddit.com/r/" + myInput + "/.rss")
+	print_rss(d)
+	
 #prints the title and URL for the first 10 RSS entries 
 def print_rss(parse_results):
+	
 	index = 1
-
 	for post in parse_results.entries[0:10]:
 		print(str(index) + ") " + post.title)
 		#print post.title
@@ -14,15 +24,12 @@ def print_rss(parse_results):
 
 		index = index + 1
 
-def subreddit(myInput):
-	d = feedparser.parse("http://www.reddit.com/r/" + myInput + "/.rss")
-	print_rss(d)	
-
-	
+#Unused function that allows sports RSS feeds to be parsed and displayed
 def sports(myInput):
 	d = feedparser.parse("http://sports.espn.go.com/espn/rss/" + myInput + "/news")
 	print_rss(d)
  
+#Unused function that allows traffic RSS feeds to be parsed and displayed
 def traffic(myInput):
 	base_url = "http://traffic.houstontranstar.org/data/rss/traffic_rss_"
 	
